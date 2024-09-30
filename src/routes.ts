494ff2a4bef1a70f7eee061a0ef7b608b23d6ec8
Asif <txxasif@gmail.com>
2024-09-30 21:58:13 +0600

    elastic search

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Application } from 'express';
 import { healthRoutes } from '@auth/routes/health';
 import { authRoutes } from '@auth/routes/auth';
 import { currentUserRoutes } from '@auth/routes/current-user';
+import { searchRoutes } from '@auth/routes/search';
 
 const BASE_PATH = '/api/v1/auth';
 
 export function appRoutes(app: Application): void {
   app.use('', healthRoutes());
   app.use(BASE_PATH, authRoutes());
+  app.use(BASE_PATH, searchRoutes());
   app.use(BASE_PATH, currentUserRoutes());
 }
